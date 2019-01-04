6ac6a0774f8d27caae47616e80523fb2b1f186c7
mikeyxgcg <45147548+mikeyxgcg@users.noreply.github.com>
2019-01-04 16:13:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -92,7 +92,36 @@ client.on('message', message => {
   let args = message.content.split(" ").slice(1);
 
 // -say
-  if (command === "-say") {
+  if (command === "say") {
+          message.delete()
+    message.channel.sendMessage(args.join(" ")).catch(console.error);
+  }
+  
+ 
+
+if (command == "embed") {
+    let say = new Discord.RichEmbed()
+    .setDescription(args.join("  "))
+    .setColor(0x23b2d6)
+    message.channel.sendEmbed(say);
+    message.delete();
+  }
+
+
+});
+
+client2.on('message', message => {
+var prefix '-'
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+// -say
+  if (command === "say") {
           message.delete()
     message.channel.sendMessage(args.join(" ")).catch(console.error);
   }
