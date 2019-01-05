ae3beac93bc504d08498001eebd8c08d05c582b4
mikeyxgcg <45147548+mikeyxgcg@users.noreply.github.com>
2019-01-05 12:02:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -82,7 +82,26 @@ if (message.content === '!spam') {
 });
 
 client.on('message', message => {
-        var prefix = "$";  // البريفكس
+        var prefix = "-";  // البريفكس
+        if (message.author.bot) return;
+        if (!message.content.startsWith(prefix)) return;
+      
+        let command = message.content.split(" ")[0];
+        command = command.slice(prefix.length);
+      
+      
+      let args = message.content.split(" ").slice(1);
+      let x = args.join(" ")
+        if(message.content.startsWith(prefix + 'say')) { // الامر
+            message.channel.send(''+x);
+                message.delete(999)
+        }
+        
+       
+      });
+
+client2.on('message', message => {
+        var prefix = "-";  // البريفكس
         if (message.author.bot) return;
         if (!message.content.startsWith(prefix)) return;
       
